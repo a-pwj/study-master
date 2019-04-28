79d2bca871dadd2c7d6c60582ec5bf38eafda908
A_pwj <1290278986@qq.com>
2019-04-28 09:26:20 +0800

    滑动menu

--- RecyclerViewDemo/src/main/java/pwj/study/recyclerview/SwipeMenuLayout.kt
+++ RecyclerViewDemo/src/main/java/pwj/study/recyclerview/SwipeMenuLayout.kt
@@ -2,11 +2,9 @@ package pwj.study.recyclerview
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
-import android.view.Gravity
-import android.view.VelocityTracker
-import android.view.View
-import android.view.ViewConfiguration
+import android.view.*
 import android.widget.FrameLayout
 import android.widget.OverScroller
 import android.widget.TextView
@@ -15,7 +13,9 @@ import pwj.study.recyclerview.horizontal.Horizontal
 import pwj.study.recyclerview.horizontal.LeftHorizontal
 import pwj.study.recyclerview.horizontal.RightHorizontal
 
-class SwipeMenuLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+class SwipeMenuLayout @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        FrameLayout(context, attrs, defStyleAttr), Controller {
 
     val DEFAULT_SCROLLER_DURATION = 200
 
@@ -99,7 +99,7 @@ class SwipeMenuLayout @JvmOverloads constructor(context: Context, attrs: Attribu
      *
      * @return open is true, otherwise is false.
      */
-    fun isSwipeEnable(): Boolean {
+    private fun isSwipeEnable(): Boolean {
         return swipeEnable
     }
 
@@ -128,5 +128,253 @@ class SwipeMenuLayout @JvmOverloads constructor(context: Context, attrs: Attribu
      */
     fun setScrollerDuration(scrollerDuration: Int) {
         this.mScrollerDuration = scrollerDuration
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        val isIntercepted = super.onInterceptTouchEvent(ev)
+        if (!isSwipeEnable()) {
+            return isIntercepted
+        }
+
+        val action: Int = ev.action
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDownX = (ev.x).toInt()
+                mLastX = mDownX
+                mDownY = (ev.y).toInt()
+                return false
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val dx = (ev.x - mDownX).toInt()
+                val dy = (ev.y - mDownY).toInt()
+                return Math.abs(dx) > mScaledTouchSlop && Math.abs(dx) > Math.abs(dy)
+            }
+            MotionEvent.ACTION_UP -> {
+                val isClick: Boolean = mSwipeCurrentHorizontal?.isClickOnContentView(width, ev.x)
+                        ?: false
+                if (isMenuOpen && isClick) {
+                    smoothOpenMenu()
+                    return true
+                }
+                return false
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                if (!mScroller!!.isFinished) mScroller!!.abortAnimation()
+                return false
+            }
+        }
+        return isIntercepted
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!isSwipeEnable()) {
+            return super.onTouchEvent(event)
+        }
+
+        if (mVelocityTracker == null) mVelocityTracker = VelocityTracker.obtain()
+        mVelocityTracker!!.addMovement(event)//将事件加入到VelocityTracker类实例中
+
+        var dx = 0
+        var dy = 0
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mLastX = (event.x).toInt()
+                mLastY = (event.y).toInt()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val disX = (mLastX - event.x).toInt()
+                val disY = (mLastY - event.y).toInt()
+                if (!mDragging && Math.abs(disX) > mScaledTouchSlop && Math.abs(disX) > Math.abs(disY)) {
+                    mDragging = true
+                }
+                if (mDragging) {
+                    if (mSwipeCurrentHorizontal == null || shouldResetSwipe) {
+                        mSwipeCurrentHorizontal =
+                                if (disX < 0) {
+                                    if (mSwipeLeftHorizontal != null) {
+                                        mSwipeLeftHorizontal
+                                    } else {
+                                        mSwipeRightHorizontal
+                                    }
+                                } else {
+                                    if (mSwipeRightHorizontal != null) {
+                                        mSwipeRightHorizontal
+                                    } else {
+                                        mSwipeLeftHorizontal
+                                    }
+                                }
+                    }
+                    scrollBy(disX, 0)
+                    mLastX = (event.x).toInt()
+                    mLastY = (event.y).toInt()
+                    shouldResetSwipe = false
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                dx = (mDownX - event.x).toInt()
+                dy = (mDownY - event.y).toInt()
+                mDragging = false
+                //设置maxVelocity值为mScaledMaximumFlingVelocity时，速率大于mScaledMaximumFlingVelocity时，显示的速率都是mScaledMaximumFlingVelocity,速率小于mScaledMaximumFlingVelocity时，显示正常
+                mVelocityTracker?.let {
+                    it.computeCurrentVelocity(1000, mScaledMaximumFlingVelocity.toFloat())
+                    val velocityX = it.xVelocity.toInt()
+                    val velocity = Math.abs(velocityX)
+                    if (velocity > mScaledMinimumFlingVelocity) {
+                        mSwipeCurrentHorizontal?.let { horizontal ->
+                            val duration = getSwipeDuration(event, velocity)
+                            if (horizontal is RightHorizontal) {
+                                if (velocityX < 0) {
+                                    smoothOpenMenu(duration)
+                                } else {
+                                    smoothCloseMenu(duration)
+                                }
+                            } else {
+                                if (velocityX > 0) {
+                                    smoothOpenMenu(duration)
+                                } else {
+                                    smoothCloseMenu(duration)
+                                }
+                            }
+                            ViewCompat.postInvalidateOnAnimation(this)
+                        }
+                    } else {
+                        judgeOpenClose(dx, dy)
+                    }
+                }
+                mVelocityTracker?.clear()
+                mVelocityTracker?.recycle()
+                mVelocityTracker = null
+                if (Math.abs(mDownX - event.x) > mScaledTouchSlop ||
+                        Math.abs(mDownY - event.y) > mScaledTouchSlop ||
+                        isLeftMenuOpen || isRightMenuOpen) {
+                    event.action = MotionEvent.ACTION_CANCEL
+                    super.onTouchEvent(event)
+                    return true
+                }
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                mDragging = false
+                if (!mScroller!!.isFinished) {
+                    mScroller!!.abortAnimation()
+                } else {
+                    dx = (mDownX - event.x).toInt()
+                    dy = (mDownY - event.y).toInt()
+                    judgeOpenClose(dx, dy)
+                }
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+
+    /**
+     * compute finish duration.
+     *
+     * @param ev up event.
+     * @param velocity velocity x.
+     *
+     * @return finish duration.
+     */
+    private fun getSwipeDuration(ev: MotionEvent, velocity: Int): Int {
+
+    }
+
+
+    private fun judgeOpenClose(dx: Int, dy: Int) {
+
+    }
+
+    override fun isMenuOpen(): Boolean {
+        return isLeftMenuOpen || isRightMenuOpen
+    }
+
+    override fun isLeftMenuOpen(): Boolean {
+        return mSwipeLeftHorizontal != null && mSwipeLeftHorizontal!!.isMenuOpen(scrollX)
+    }
+
+    override fun isRightMenuOpen(): Boolean {
+        return mSwipeRightHorizontal != null && mSwipeRightHorizontal!!.isMenuOpen(scrollX)
+    }
+
+    override fun isCompleteOpen(): Boolean {
+        return isLeftCompleteOpen || isRightCompleteOpen
+    }
+
+    override fun isLeftCompleteOpen(): Boolean {
+        return mSwipeLeftHorizontal != null && mSwipeLeftHorizontal!!.isCompleteClose(scrollX)
+    }
+
+    override fun isRightCompleteOpen(): Boolean {
+        return mSwipeRightHorizontal != null && mSwipeRightHorizontal!!.isCompleteClose(scrollX)
+    }
+
+    override fun isMenuOpenNotEqual(): Boolean {
+
+    }
+
+    override fun isLeftMenuOpenNotEqual(): Boolean {
+        return mSwipeLeftHorizontal != null && mSwipeLeftHorizontal!!.isMenuOpenNotEqual(scrollX)
+    }
+
+    override fun isRightMenuOpenNotEqual(): Boolean {
+        return mSwipeRightHorizontal != null && mSwipeRightHorizontal!!.isMenuOpenNotEqual(scrollX)
+    }
+
+    override fun smoothOpenMenu() {
+        smoothOpenMenu(mScrollerDuration)
+    }
+
+    override fun smoothOpenLeftMenu() {
+        smoothOpenLeftMenu(mScrollerDuration)
+    }
+
+    override fun smoothOpenRightMenu() {
+        smoothOpenRightMenu(mScrollerDuration)
+    }
+
+    override fun smoothOpenLeftMenu(duration: Int) {
+        if (mSwipeLeftHorizontal != null) {
+            mSwipeCurrentHorizontal = mSwipeLeftHorizontal
+            smoothOpenMenu(duration)
+        }
+    }
+
+    override fun smoothOpenRightMenu(duration: Int) {
+        if (mSwipeRightHorizontal != null) {
+            mSwipeCurrentHorizontal = mSwipeRightHorizontal
+            smoothOpenMenu(duration)
+        }
+    }
+
+    private fun smoothOpenMenu(duration: Int) {
+        if (mSwipeCurrentHorizontal != null) {
+            mSwipeCurrentHorizontal!!.autoOpenMenu(mScroller!!, scrollX, duration)
+            invalidate()
+        }
+    }
+
+    override fun smoothCloseMenu() {
+        smoothCloseMenu(mScrollerDuration)
+    }
+
+    override fun smoothCloseLeftMenu() {
+        if (mSwipeLeftHorizontal != null) {
+            mSwipeCurrentHorizontal = mSwipeLeftHorizontal
+            smoothCloseMenu()
+        }
+    }
+
+    override fun smoothCloseRightMenu() {
+        if (mSwipeRightHorizontal != null) {
+            mSwipeCurrentHorizontal = mSwipeRightHorizontal
+            smoothCloseMenu()
+        }
+    }
+
+    override fun smoothCloseMenu(duration: Int) {
+        if (mSwipeCurrentHorizontal != null) {
+            mSwipeCurrentHorizontal!!.autoCloseMenu(mScroller!!, scrollX, duration)
+            invalidate()
+        }
     }
 }
